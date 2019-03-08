87a8d3a42f6bbb00526769ef86c95fe571dd2082
louis <hongfeng@qianxiangbank.com>
2019-03-08 15:16:43 +0800

    up

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,42 @@
 // JavaScript Document
-function preloader() {  
-    if (document.images) {  
-        var img1 = new Image();  
-        var img2 = new Image();  
-        var img3 = new Image(); 
-        var img4 = new Image(); 
-        var img5 = new Image(); 
-        var img6 = new Image(); 
-        var img7 = new Image(); 
-        var img8 = new Image(); 
-        img1.src = "img/portfolio/07-1.gif";  
-        img2.src = "img/portfolio/07-2.gif";  
+function preloader() {
+    if (document.images) {
+        var img1 = new Image();
+        var img2 = new Image();
+        var img3 = new Image();
+        var img4 = new Image();
+        var img5 = new Image();
+        var img6 = new Image();
+        var img7 = new Image();
+        var img8 = new Image();
+        img1.src = "img/portfolio/07-1.gif";
+        img2.src = "img/portfolio/07-2.gif";
         img3.src = "img/portfolio/01-1.jpg";
         img4.src = "img/portfolio/02.jpg";
         img5.src = "img/portfolio/03-1.jpg";
         img6.src = "img/portfolio/05-1.jpg";
         img7.src = "img/portfolio/06-1.jpg";
         img8.src = "img/portfolio/08-1.jpg";
-    }  
-}  
-function addLoadEvent(func) {  
-    var oldonload = window.onload;  
-    if (typeof window.onload != 'function') {  
-        window.onload = func;  
-    } else {  
-        window.onload = function() {  
-            if (oldonload) {  
-                oldonload();  
-            }  
-            func();  
-        }  
-    }  
-}  
-addLoadEvent(preloader);
+    }
+}
 
-$(window).load(function () {
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+//
+// addLoadEvent(preloader);
+
+$(document).ready(function () {
     "use strict";
     // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
@@ -43,15 +45,16 @@ $(window).load(function () {
         'overflow': 'visible'
     });
 })
-$(document).on("contextmenu",".portfolio-padding",function(){
-	return false;
+
+$(document).on("contextmenu", ".portfolio-padding", function () {
+    return false;
 })
- $(function () {
-          $(".portfolio-padding").bind("click", function (e) {
-         // 	alert(1)
-              //return false;
-          });
-  });
+$(function () {
+    $(".portfolio-padding").bind("click", function (e) {
+        // 	alert(1)
+        //return false;
+    });
+});
 $(document).ready(function () {
     "use strict";
 
@@ -104,7 +107,6 @@ $(document).ready(function () {
     });
 
 
-
     // Parallax
     var parallax = function () {
         $(window).stellar();
@@ -146,41 +148,59 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $(".portfolio_item").click(function(){
-    	$("#animatedModal .container").empty();
-    	var oId = $(this).attr("id");
-    	$("#animatedModal").scrollTop(0)
-    	switch (oId){
-    		case "demo01":
-    			$("#modalDome01").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo02":
-    			$("#modalDome02").clone().prependTo("#animatedModal .container");
-    			break;	
-    		case "demo03":
-    			$("#modalDome03").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo04":
-    			$("#modalDome04").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo05":
-    			$("#modalDome05").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo06":
-    			$("#modalDome06").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo07":
-    			$("#modalDome07").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo08":
-    			$("#modalDome08").clone().prependTo("#animatedModal .container");
-    			break;
-    		case "demo09":
-    			$("#modalDome09").clone().prependTo("#animatedModal .container");
-    			break;	
-    		default:
-    			break;
-    	}
+    $(".portfolio_item").click(function () {
+        $("#animatedModal .container").empty();
+        var oId = $(this).attr("id");
+        $("#animatedModal").scrollTop(0)
+        switch (oId) {
+            case "demo01":
+                var src = $('#modalDome01 .img-responsive').attr('data-src');
+                $('#modalDome01 .img-responsive').attr('src', src);
+                $("#modalDome01").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo02":
+                var src = $('#modalDome02 .img-responsive').attr('data-src');
+                $('#modalDome02 .img-responsive').attr('src', src);
+                $("#modalDome02").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo03":
+                var src = $('#modalDome03 .img-responsive').attr('data-src');
+                $('#modalDome03 .img-responsive').attr('src', src);
+                $("#modalDome03").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo04":
+                var src = $('#modalDome04 .img-responsive').attr('data-src');
+                $('#modalDome04 .img-responsive').attr('src', src);
+                $("#modalDome04").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo05":
+                var src = $('#modalDome05 .img-responsive').attr('data-src');
+                $('#modalDome05 .img-responsive').attr('src', src);
+                $("#modalDome05").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo06":
+                var src = $('#modalDome06 .img-responsive').attr('data-src');
+                $('#modalDome06 .img-responsive').attr('src', src);
+                $("#modalDome06").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo07":
+                var src = $('#modalDome07 .img-responsive').attr('data-src');
+                $('#modalDome07 .img-responsive').attr('src', src);
+                $("#modalDome07").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo08":
+                var src = $('#modalDome08 .img-responsive').attr('data-src');
+                $('#modalDome08 .img-responsive').attr('src', src);
+                $("#modalDome08").clone().prependTo("#animatedModal .container");
+                break;
+            case "demo09":
+                var src = $('#modalDome09 .img-responsive').attr('data-src');
+                $('#modalDome09 .img-responsive').attr('src', src);
+                $("#modalDome09").clone().prependTo("#animatedModal .container");
+                break;
+            default:
+                break;
+        }
     })
     $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
 
